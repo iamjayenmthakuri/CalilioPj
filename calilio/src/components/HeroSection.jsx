f2fdf7defa2ea938f275a59dad4eb1c5a93c8b4e
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-29 23:15:18 +0545

    last commit of today

--- calilio/src/components/HeroSection.jsx
+++ calilio/src/components/HeroSection.jsx
@@ -15,9 +15,15 @@ import Contacts from "../Images/Contacts.svg";
 import setting from "../Images/setting.svg";
 import sms from "../Images/sms.svg";
 import Phone from "../Images/chat.svg";
-import activeprofile from "../activeprofie.svg";
-
+import activeprofile from "../Images/activeprofile.svg";
 import { ChevronDownIcon, AddIcon } from "@chakra-ui/icons";
+import {
+  toggleHideModeState,
+  hideModeState,
+  toggleSecondHideModeState,
+  secondHideState,
+} from "../helpers/GobalState";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 
 const StyledFlex = styled(Flex)`
   align-items: center;
@@ -50,9 +56,45 @@ const StyledBox = styled(Box)`
   border-radius: 8px;
 `;
 
+const StyledIcon = styled(ChevronDownIcon)`
+  &:hover {
+    color: #000000;
+    cursor: pointer;
+  }
+  &:active {
+    transform: rotate(180deg);
+  }
+`;
+const Wrapper = styled.div`
+  display: "flex";
+  background-color: white;
+  justify-content: center;
+
+  align-items: "center";
+  &:active ${StyledIcon} {
+    transform: rotate(180deg);
+  }
+  &:hover ${StyledIcon} {
+    color: #000000;
+    cursor: pointer;
+  }
+`;
+
 function HeroSection() {
+  const isHideModeEnabaled = useRecoilValue(hideModeState);
+  const isSecondHideModeEnabaled = useRecoilValue(secondHideState);
+
+  const toggleHideMode = useSetRecoilState(toggleHideModeState);
+  const toggleSecondHideMode = useSetRecoilState(toggleSecondHideModeState);
+
   return (
-    <StyledBox width=" 280px" height="88vh" bgColor="white" padding="2rem">
+    <StyledBox
+      width=" 280px"
+      height=" 992px"
+      bgColor="white"
+      padding="2rem"
+      marginBottom="8px"
+    >
       <List>
         <ListItem>
           <StyledFlex>
@@ -90,42 +132,57 @@ function HeroSection() {
           backgroundColor="white"
           marginTop="24px"
         >
-          <ChevronDownIcon w="18px" backgroundColor="white" />
-          <Text w="200px" backgroundColor="white" fontWeight="600" pl="2px">
-            Numbers
-          </Text>
+          <Wrapper>
+            <StyledIcon
+              w="18px"
+              backgroundColor="white"
+              onClick={toggleHideMode}
+            />
+            <Text
+              w="200px"
+              backgroundColor="white"
+              fontWeight="600"
+              pl="2px"
+              onClick={toggleHideMode}
+            >
+              Numbers
+            </Text>
+          </Wrapper>
           <AddIcon w="15px" backgroundColor="white" />
         </Flex>
-        <SimpleGrid
-          outline=" 1.5px solid #e4e1e1"
-          borderRadius=" 8px "
-          marginTop="18px"
-          height="100px"
-          minChildWidth="220px"
-          placeItems={"center"}
-          backgroundColor="white"
-        >
-          <Image src={Phone} backgroundColor="white" />
-          <Text
-            fontSize="12px"
+        {isHideModeEnabaled ? (
+          <SimpleGrid
+            transition="height 0.9s ease-in-out"
+            outline=" 1.5px solid #e4e1e1"
+            borderRadius=" 8px "
+            marginTop="18px"
+            height="100px"
+            minChildWidth="220px"
+            placeItems={"center"}
             backgroundColor="white"
-            color="#667085"
-            fontWeight="400px"
           >
-            You don't have any numbers in this
-          </Text>
-          <Text
-            fontSize="12px"
-            backgroundColor="white"
-            color="#667085"
-            fontWeight="400px"
-          >
-            workspace.
-            <Text as="span" color="blue" backgroundColor="white">
-              Purchase Number
+            <Image src={Phone} backgroundColor="white" />
+            <Text
+              fontSize="12px"
+              backgroundColor="white"
+              color="#667085"
+              fontWeight="400px"
+            >
+              You don't have any numbers in this
             </Text>
-          </Text>
-        </SimpleGrid>
+            <Text
+              fontSize="12px"
+              backgroundColor="white"
+              color="#667085"
+              fontWeight="400px"
+            >
+              workspace.
+              <Text as="span" color="blue" backgroundColor="white">
+                Purchase Number
+              </Text>
+            </Text>
+          </SimpleGrid>
+        ) : null}
         <Flex
           w="100%"
           align="center"
@@ -133,14 +190,39 @@ function HeroSection() {
           backgroundColor="white"
           marginTop="26px"
         >
-          <ChevronDownIcon w="18px" backgroundColor="white" />
-          <Text w="200px" backgroundColor="white" fontWeight="600" pl="2px">
-            Members
-          </Text>
+          {" "}
+          <Wrapper>
+            <StyledIcon
+              w="18px"
+              backgroundColor="white"
+              onClick={toggleSecondHideMode}
+            />
+            <Text
+              w="200px"
+              backgroundColor="white"
+              fontWeight="600"
+              pl="2px"
+              onClick={toggleSecondHideMode}
+            >
+              Members
+            </Text>
+          </Wrapper>
           <AddIcon w="15px" backgroundColor="white" />
         </Flex>
       </Box>
-      <Image src={activeprofile} />
+      {isSecondHideModeEnabaled ? (
+        <Flex
+          gap="10px"
+          marginTop="20px"
+          align="center"
+          backgroundColor="white"
+        >
+          <Image src={activeprofile} backgroundColor="white" />
+          <Text fontSize="13px" fontWeight="500" backgroundColor="white">
+            Olivia Maiden Rhye (You){" "}
+          </Text>
+        </Flex>
+      ) : null}
     </StyledBox>
   );
 }
--- calilio/src/main.jsx
+++ calilio/src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
+import { RecoilRoot } from "recoil";
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
