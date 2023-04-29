689d7b4594fcb5bb1d0e9aa4c3a0b1221464231c
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-29 20:27:27 +0545

    cmpleting two section of hero section

--- calilio/src/components/HeroSection.jsx
+++ calilio/src/components/HeroSection.jsx
@@ -1,10 +1,21 @@
-import { Box, List, ListItem, Image, Flex } from "@chakra-ui/react";
+/* eslint-disable react/no-unescaped-entities */
+import {
+  Box,
+  List,
+  ListItem,
+  Image,
+  Flex,
+  Text,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Dashboard from "../Images/dashbord.svg";
 import Contacts from "../Images/Contacts.svg";
 import setting from "../Images/setting.svg";
 import sms from "../Images/sms.svg";
+import Phone from "../Images/chat.svg";
+import { ChevronDownIcon, AddIcon } from "@chakra-ui/icons";
 
 const StyledFlex = styled(Flex)`
   align-items: center;
@@ -31,7 +42,7 @@ const StyledLink = styled(Link)`
 `;
 
 const StyledBox = styled(Box)`
-  background: #ffffff;
+  background-color: white;
   margin-top: 8px;
   box-shadow: inset -1px 0px 0px rgba(0, 0, 0, 0.25);
   border-radius: 8px;
@@ -69,6 +80,64 @@ function HeroSection() {
           </StyledFlex>
         </ListItem>
       </List>
+      <Box background="white">
+        <Flex
+          w="100%"
+          align="center"
+          color="rgba(52, 64, 84, 1)"
+          backgroundColor="white"
+          marginTop="24px"
+        >
+          <ChevronDownIcon w="18px" backgroundColor="white" />
+          <Text w="200px" backgroundColor="white" fontWeight="600" pl="2px">
+            Numbers
+          </Text>
+          <AddIcon w="15px" backgroundColor="white" />
+        </Flex>
+        <SimpleGrid
+          outline=" 1.5px solid #e4e1e1"
+          borderRadius=" 8px "
+          marginTop="18px"
+          height="100px"
+          minChildWidth="220px"
+          placeItems={"center"}
+          backgroundColor="white"
+        >
+          <Image src={Phone} backgroundColor="white" />
+          <Text
+            fontSize="12px"
+            backgroundColor="white"
+            color="#667085"
+            fontWeight="400px"
+          >
+            You don't have any numbers in this
+          </Text>
+          <Text
+            fontSize="12px"
+            backgroundColor="white"
+            color="#667085"
+            fontWeight="400px"
+          >
+            workspace.
+            <Text as="span" color="blue" backgroundColor="white">
+              Purchase Number
+            </Text>
+          </Text>
+        </SimpleGrid>
+        <Flex
+          w="100%"
+          align="center"
+          color="rgba(52, 64, 84, 1)"
+          backgroundColor="white"
+          marginTop="26px"
+        >
+          <ChevronDownIcon w="18px" backgroundColor="white" />
+          <Text w="200px" backgroundColor="white" fontWeight="600" pl="2px">
+            Members
+          </Text>
+          <AddIcon w="15px" backgroundColor="white" />
+        </Flex>
+      </Box>
     </StyledBox>
   );
 }
